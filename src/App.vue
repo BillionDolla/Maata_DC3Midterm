1e86503f54cfc1dbe06beb9bfc1984d6f06e3b59
https://github.com/johnmarkorongan53-art <johnmarkorongan53@gmail.com>
2026-08-25 09:55:19 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<script setup>
+import { ref } from 'vue'
+import StudentForm from './StudentForm.vue'
+import StudentDisplay from './StudentDisplay.vue'
+
+const student = ref(null)
+
+function receiveStudent(data) {
+  student.value = data
+}
+</script>
+
+<template>
+  <div>
+    <h1>Student Information System</h1>
+
+    <StudentForm @student-submitted="receiveStudent" />
+
+    <StudentDisplay :student="student" />
+  </div>
+</template>
